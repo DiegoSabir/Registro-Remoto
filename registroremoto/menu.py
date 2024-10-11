80e217d654acb7de3805b1f5aab646abb3967939
Abel Rodríguez Pastoriza <abel.lisensiado@gmail.com>
2024-10-11 13:01:52 +0200

    actualizacion boton

--- registroremoto/menu.py
+++ registroremoto/menu.py
@@ -10,39 +10,55 @@ def menu_view(page: ft.Page, uid, password, employee_id):
 
     # Crear el contenido del menú
     page.add(
-        ft.Container(
-            content=ft.Text("Bienvenido al menú de asistencias", size=30, weight="bold"),
-            alignment=ft.alignment.center
-        )
+    ft.Container(
+        content=ft.Text("Bienvenido al menú de asistencia", size=24, weight="bold", color="blue"),
+        alignment=ft.alignment.top_center,
+        margin=40  # Cambiado para usar un diccionario
     )
+)
 
     # Crear botones de fichaje
     entrada_button = ft.ElevatedButton(
-        text="Fichar Entrada",
-        on_click=lambda e: gestionar_fichaje(uid, password, employee_id, 'entrada', entrada_button, salida_button, page)
+        text="🕘 Fichar Entrada",
+        on_click=lambda e: gestionar_fichaje(uid, password, employee_id, 'entrada', entrada_button, salida_button, page),
+        style=ft.ButtonStyle(
+            padding=ft.padding.symmetric(vertical=40, horizontal=60),
+            shape=ft.RoundedRectangleBorder(20),  # Botones con esquinas redondeadas
+            bgcolor=('#28A745'),
+            color=('#FFFFFF'),
+        )
     )
 
     salida_button = ft.ElevatedButton(
-        text="Fichar Salida",
-        on_click=lambda e: gestionar_fichaje(uid, password, employee_id, 'salida', entrada_button, salida_button, page)
+        text="🚪 Fichar Salida",
+        on_click=lambda e: gestionar_fichaje(uid, password, employee_id, 'salida', entrada_button, salida_button, page),
+        style=ft.ButtonStyle(
+            padding=ft.padding.symmetric(vertical=40, horizontal=60), 
+            shape=ft.RoundedRectangleBorder(20),  # Botones con esquinas redondeadas
+            bgcolor=('#DC3545'),
+            color=('#FFFFFF'),
+            
+        )
     )
 
     # Mostrar solo el botón correspondiente según la verificación
     if ya_fichando:
-        # El usuario ya ha fichado, mostrar solo el botón de salida
         salida_button.visible = True
         entrada_button.visible = False  # Ocultar el de entrada
     else:
-        # El usuario no ha fichado, mostrar solo el botón de entrada
         entrada_button.visible = True
-        salida_button.visible = False  # Ocultar el de salida
+        salida_button.visible = False  
 
-    if ya_fichando:
-        page.add(salida_button)
-        page.add(entrada_button)
-    else:
-        page.add(entrada_button)
-        page.add(salida_button)
+    # Añadir los botones a la página
+    page.add(
+        ft.Row(
+            controls=[entrada_button, salida_button],
+            alignment=ft.MainAxisAlignment.CENTER,
+            spacing=20,  # Espacio entre botones
+            wrap=True # Permite que los botones se ajusten si la pantalla es pequeña
+        )
+    )
 
     # Actualizar la página al final
     page.update()
+
